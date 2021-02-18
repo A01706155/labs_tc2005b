c36d97c51abbae8e836ae294e84e00ae4a2b4129
Manolo Ramírez Pintor <A01706155@itesm.mx>
2021-02-17 23:54:49 -0600

    Lab 5 terminado

--- laboratorio_5/js/lab_5.js
+++ laboratorio_5/js/lab_5.js
@@ -6,8 +6,8 @@
 //ej6()
 
 function validar(){
-    var password = document.getElementById("contrasena").value;
-    var confirmPassword = document.getElementById("contrasena2").value;
+    let password = document.getElementById("contrasena").value;
+    let confirmPassword = document.getElementById("contrasena2").value;
     if (password == ""){
         alert("¡Escribe algo en el campo de contraseñas!");
     }
@@ -19,8 +19,24 @@ function validar(){
         }
         else{
             document.getElementById("passback").style.color = "yellowgreen";
-            document.getElementById("passback").innerHTML = "¡Contraseña validada!"
+            document.getElementById("passback").innerHTML = "¡Contraseña validada!";
             return true;
         }
+    }
+}
+
+function checkout(){
+
+    let dc = (document.getElementById("dreamcast").value);
+    let pc = (document.getElementById("pc").value);
+    let xone = (document.getElementById("xone").value);
+
+    let total = (dc*199) + (pc*699) + (xone*399);
+
+    if (dc+pc+xone == 0){
+        alert("No has comprado ningún producto.")
+    }
+    else {
+        document.getElementById("confirmacion").innerHTML = "Has comprado " + dc + " Dreamcasts, " + pc + " PCs Gamer y " + xone + " Xbox ones por " + total + " dólares.";
     }
 }
