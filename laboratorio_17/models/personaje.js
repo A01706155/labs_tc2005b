42ec90b8cd7b6624fb800518588f07d40044f95b
Manolo Ramírez Pintor <A01706155@itesm.mx>
2021-04-10 22:29:45 -0500

    Conexion establecida BD, arreglo y recorrido variables

--- laboratorio_17/models/personaje.js
+++ laboratorio_17/models/personaje.js
@@ -1,8 +1,4 @@
-const personajes = [
-    {nombre: "Pico", imagen: "https://media.tenor.com/images/889572ee13c6a40d31607d9b350112a8/tenor.png"},
-    {nombre: "Girlfriend", imagen: "https://images.gamebanana.com/img/ss/srends/5ff153b3b0f3e.jpg"}, 
-    {nombre: "Dad", imagen: "<script>alert('You haven been hacked by Dad')</script>"},
-    {nombre: "Mom", imagen: "https://i.pinimg.com/originals/0b/58/93/0b5893adca32845610b14c28e30466f8.jpg"}];
+const db = require('../util/database');
 
 module.exports = class Personaje {
 
@@ -19,6 +15,22 @@ module.exports = class Personaje {
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
     static fetchAll() {
+        
+        const personajes = [];
+        
+        db.execute('SELECT * FROM personajes')
+            .then(([rows, fieldData]) => {
+                for (let personaje of rows){
+                    // console.log(personaje.nombre);
+                    personajes.push({nombre: personaje.nombre,
+                                     imagen: personaje.imagen});
+                }
+                // console.log(rows);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+            
         return personajes;
     }
 
